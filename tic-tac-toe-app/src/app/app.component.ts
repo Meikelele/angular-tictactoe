5e1093461ab07642292e47a552d8f27e9915e158
Meikelele <michal.bak2002@interia.pl>
2025-07-11 23:16:20 +0200

    deleteing unnecessary stuffs

--- tic-tac-toe-app/src/app/app.component.ts
+++ tic-tac-toe-app/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SquareComponent } from "./square/square.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SquareComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
